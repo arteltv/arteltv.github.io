fc64addcecf172cfd6e2a68123b281612bb14f5d
arteltv <65528471+arteltv@users.noreply.github.com>
2020-05-29 10:11:11 +0500

    new site

---
+++ demo/sendemail.php
@@ -0,0 +1,18 @@
+<?php
+$name       = @trim(stripslashes($_POST['name'])); 
+$from       = @trim(stripslashes($_POST['email'])); 
+$subject    = @trim(stripslashes($_POST['subject'])); 
+$message    = @trim(stripslashes($_POST['message'])); 
+$to   		= 'arteltv.axangaran@gmail.com';//replace with your email
+
+$headers   = array();
+$headers[] = "MIME-Version: 1.0";
+$headers[] = "Content-type: text/plain; charset=utf-8\r\n";
+$headers[] = "From: {$name} <{$from}>";
+$headers[] = "Reply-To: <{$from}>";
+$headers[] = "Subject: {$subject}";
+$headers[] = "X-Mailer: PHP/".phpversion();
+
+mail($to, $subject, $message, $headers);
+
+die;
